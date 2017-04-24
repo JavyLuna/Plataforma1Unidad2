2ca79210e78642114dd541aff6fd83fcb214d1fd
JavyLuna <javy_luna_jr@hotmail.com>
2017-04-24 10:45:43 -0600

    Update ViewController.swift

--- 2DA_EVA_8_CERRADURA/2DA_EVA_8_CERRADURA/ViewController.swift
+++ 2DA_EVA_8_CERRADURA/2DA_EVA_8_CERRADURA/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+//Variables globales     
     var aiImagenes: [UIImage]!
     var password = [1,1,4]
     var def = [0,0,0]
-    
+//Arreglo con los nombres de las imagenes    
     let aiImagenes1 = [
         "facebook",
         "google",
         "instagram",
         "pinterest",
         "twitter-xl"]
-    
+//Arreglo con los nombres de las imagenes     
     override func viewDidLoad() {
         super.viewDidLoad()
         aiImagenes = [UIImage(named: "facebook")!,
@@ -35,15 +36,15 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+//Nos muestra los 3 pickers con las imagenes cargadas con anterioridad     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 3
     }
-    
+ //Nos regresa las imagenes   
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return aiImagenes1[row]
     }
-    
+ //Funcion que evalua, si la combinacion de imagenes es la correcta nos mostrara de que iniciaste sesion   
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         def[component] = row
         
@@ -56,7 +57,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         
     }
-    
+  //Nos regresa las imagenes   
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return aiImagenes1.count
     }
