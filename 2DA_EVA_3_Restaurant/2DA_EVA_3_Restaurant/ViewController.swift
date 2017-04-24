3457edb84a197a9c24234571b99df5c070b74b6b
JavyLuna <javy_luna_jr@hotmail.com>
2017-04-24 10:18:32 -0600

    Update ViewController.swift

--- 2DA_EVA_3_Restaurant/2DA_EVA_3_Restaurant/ViewController.swift
+++ 2DA_EVA_3_Restaurant/2DA_EVA_3_Restaurant/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//Creamos 2 arerglos, uno que guarda el nombre de los restaurantes y el otro guarda el nombre de las imagenes puestas en los Assets    
     let misDatos = ["Barra fina","Bourke Street Bakery","Cafe Deadend","Cafe Loisl","Cafe Lore","Confessional","Donostia","Fiveleaves","Forkee Restaurant","Grahamavenuemeats","Haighschocolate","Homei","Palominoespresso","Petiteoyster","Posatelier","Royaloak","Teakha","Thai Cafe","Traif","Upstate","Waffle Wolf"]
     let imagenes = ["barrafina","bourkestreetbakery","cafedeadend","cafeloisl","cafelore","confessional","donostia","fiveleaves","forkeerestaurant","grahamavenuemeats","haighschocolate","homei","palominoespresso","petiteoyster","posatelier","royaloak","teakha","thaicafe","traif","upstate","wafflewolf"]
     
@@ -21,11 +22,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+//Esta funcion nos regresa los datos del arreglo con los nombres de los restaurantes    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return misDatos.count
     }
-    
+//Nos muestra un mensaje que el nombre del restaurante precionado    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let controlador = UIAlertController(title: "Restaurant", message: misDatos[indexPath.row], preferredStyle: .Alert)
         let boton = UIAlertAction (title: "Okay", style: .Default, handler: nil)
@@ -33,7 +34,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         presentViewController(controlador, animated: true, completion: nil)
         
     }
-    
+//Cambia la imagen al momento de precionar algun nombre de restaurante    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let celda = tableView.dequeueReusableCellWithIdentifier("micelda")
         celda?.textLabel?.text = misDatos[indexPath.row]
