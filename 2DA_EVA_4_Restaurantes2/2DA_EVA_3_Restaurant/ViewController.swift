78be9c9a6c9f90f778cd53a2c4ceb1ec335cb2a3
JavyLuna <javy_luna_jr@hotmail.com>
2017-04-24 10:22:08 -0600

    Update ViewController.swift

--- 2DA_EVA_4_Restaurantes2/2DA_EVA_3_Restaurant/ViewController.swift
+++ 2DA_EVA_4_Restaurantes2/2DA_EVA_3_Restaurant/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//Creamos los arreglos, uno con el nombre de los restaurantes y otro con el nombre de las imagenes     
     let misDatos = ["Barra fina","Bourke Street Bakery","Cafe Deadend","Cafe Loisl","Cafe Lore","Confessional","Donostia","Fiveleaves","Forkee Restaurant","Grahamavenuemeats","Haighschocolate","Homei","Palominoespresso","Petiteoyster","Posatelier","Royaloak","Teakha","Thai Cafe","Traif","Upstate","Waffle Wolf"]
     let imagenes = ["barrafina","bourkestreetbakery","cafedeadend","cafeloisl","cafelore","confessional","donostia","fiveleaves","forkeerestaurant","grahamavenuemeats","haighschocolate","homei","palominoespresso","petiteoyster","posatelier","royaloak","teakha","thaicafe","traif","upstate","wafflewolf"]
     
@@ -21,11 +22,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+//Nos regresa los datos del arreglo con los nombres del restaurante     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return misDatos.count
     }
-    
+//Funcion que nos muestra un mensaje en pantalla, mostrandonos el nombre y un boton    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let controlador = UIAlertController(title: "Restaurant", message: misDatos[indexPath.row], preferredStyle: .Alert)
         let boton = UIAlertAction (title: "Okay", style: .Default, handler: nil)
@@ -33,7 +34,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         presentViewController(controlador, animated: true, completion: nil)
         
     }
-    
+//Funcion que nos modifica el TableViewCellMia, con los datos del arreglo modificando el label y las imagenes    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let celda = tableView.dequeueReusableCellWithIdentifier("micelda") as! TableViewCellMia
         
@@ -47,12 +48,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return celda
         
     }
-    
+//Al momento de deslizar un valor de nuestra tabla, nos aparecera la opcion de compartir
     func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
         let compartirAct = UITableViewRowAction(style: .Default, title: "Compartir", handler: comparteRestaurante)
         return [compartirAct]
     }
-    
+//Funcion la cual comparte los datos a alguna red social     
     func comparteRestaurante(action: UITableViewRowAction, indexPath: NSIndexPath) -> Void {
         let miTexto = "Vine a comer en " + misDatos[indexPath.row]
         let miImagen = UIImage(named: imagenes[indexPath.row])
